68860009245abed772b6b44db1e40912a069a25b
Ovidijus <ovidijus.e@gmail.com>
2022-06-14 15:31:58 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+
+import MedicationsPage from './pages/MedicationsPage';
+import PetsPage from './pages/PetsPage';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello again world</h1>
+      <Header />
+      <Switch>
+        <Route path='/medications'>
+          <MedicationsPage />
+        </Route>
+        <Route path='/'>
+          <PetsPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import css from './Header.module.css';
+
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <div className={css['header-container']}>
+        <img className={css['vet-bee-logo']} src='assets/vet-bee-logo.png' alt='' />
+        <nav className={css['main-nav']}>
+          <Link className={css['nav-item']} exact to='/'>
+            Pets
+          </Link>
+          <Link className={css['nav-item']} to='/medications'>
+            Medications
+          </Link>
+        </nav>
+      </div>
+      <div className={css['hr']}></div>
+    </header>
+  );
+}
+
+export default Header;
