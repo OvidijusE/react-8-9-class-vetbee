57825933bb31bbab3700d23c2bce9952dadf14bb
Ovidijus <ovidijus.e@gmail.com>
2022-06-15 14:11:46 +0300

    date fixed

--- src/components/PetCard/PetCard.jsx
+++ src/components/PetCard/PetCard.jsx
@@ -5,7 +5,7 @@ const PetCard = (props) => {
   return (
     <div className={css['pet-card']}>
       <h3>{props.name}</h3>
-      <p>{props.dob}</p>
+      <p>{new Date(props.dob).toLocaleString('lt-LT').split(' ')[0]}</p>
       <p>{props.client_email}</p>
       <div className='buttons'>
         <Button main>VIEW LOG</Button>
--- src/pages/AddMedsPage/AddMedsPage.js
+++ src/pages/AddMedsPage/AddMedsPage.js
@@ -7,7 +7,7 @@ const AddMedsPage = () => {
     <div>
       <h1>Add Meds Page</h1>
       <form className={css['add-meds-form']}>
-        <h2>Add meds</h2>
+        <h2>Add medications</h2>
         <label htmlFor='name'>Enter name: </label>
         <input type='text' name='' id='name' />
         <label htmlFor='description'>Enter description: </label>
