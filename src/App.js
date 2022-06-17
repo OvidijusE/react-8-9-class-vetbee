096cd9972e40e03f05521967e58d4f9ae0ee21f1
Ovidijus <ovidijus.e@gmail.com>
2022-06-17 16:24:50 +0300

    night mode not work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import ThemeContext from './components/store/themeContext';
 import AddMedsPage from './pages/AddMedsPage/AddMedsPage';
 import AddPetPage from './pages/AddPetPage/AddPetPage';
 import MedicationsPage from './pages/MedicationsPage/MedicationsPage';
@@ -9,28 +11,41 @@ import PetsPage from './pages/PetsPage/PetsPage';
 import ViewLogsPage from './pages/ViewLogsPage/ViewLogsPage';
 
 function App() {
+  const [nightMode, setNightMode] = useState(false);
+
+  function nightTheme() {
+    setNightMode(true);
+  }
+
+  const ctx = {
+    nightTheme,
+    nightMode,
+  };
+
   return (
-    <div className='App'>
-      <Header />
-      <Switch>
-        <Route path='/ViewLogsPage'>
-          <ViewLogsPage />
-        </Route>
-        <Route path='/AddMedsPage'>
-          <AddMedsPage />
-        </Route>
-        <Route path='/AddPetsPage'>
-          <AddPetPage />
-        </Route>
-        <Route path='/medications'>
-          <MedicationsPage />
-        </Route>
-        <Route exact path='/'>
-          <PetsPage />
-        </Route>
-      </Switch>
-      <Footer />
-    </div>
+    <ThemeContext.Provider value={ctx}>
+      <div className='App'>
+        <Header />
+        <Switch>
+          <Route path='/ViewLogsPage'>
+            <ViewLogsPage />
+          </Route>
+          <Route path='/AddMedsPage'>
+            <AddMedsPage />
+          </Route>
+          <Route path='/AddPetsPage'>
+            <AddPetPage />
+          </Route>
+          <Route path='/medications'>
+            <MedicationsPage />
+          </Route>
+          <Route exact path='/'>
+            <PetsPage />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,25 @@
 import css from './Header.module.css';
 
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import ThemeContext from '../store/themeContext';
+import Button from '../../UI/Button';
 
 function Header() {
+  // const value = useContext(ThemeContext);
+  const { nightMode } = useContext(ThemeContext);
+
+  const handleClick = (e) => {
+    // SetNightMode((current) => !current);
+  };
   return (
-    <header>
+    <header className={nightMode ? '#333' : '#fff'}>
       <div className={css['header-container']}>
         <img className={css['vet-bee-logo']} src='assets/vet-bee-logo.png' alt='' />
         <nav className={css['main-nav']}>
+          <Button secondary onClick={handleClick}>
+            Dark Mode
+          </Button>
           <Link className={css['nav-item']} to='/'>
             Pets
           </Link>
